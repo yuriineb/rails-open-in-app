c33dd624e6104ce74091384585eb2f9e57756af5
Calvin Yu <calvin@scoutmob.com>
2014-04-16 10:34:29 -0400

    Support if & unless options

--- lib/open_in_app/controller_helper.rb
+++ lib/open_in_app/controller_helper.rb
@@ -2,14 +2,6 @@ require 'active_support/concern'
 
 module OpenInApp
 
-  class Filter
-    def self.filter(controller)
-      if controller.send(:open_in_app?)
-        controller.send(:open_in_app) and return false
-      end
-    end
-  end
-
   module ControllerHelper
     extend ActiveSupport::Concern
 
@@ -33,7 +25,15 @@ module OpenInApp
       def open_in_app?
         open_in_app_device? &&
           open_in_app_supported_request? &&
-          open_in_app_url_present?
+          open_in_app_url_present? &&
+          (
+            self.class.open_in_app_options[:if].nil? ||
+            send(self.class.open_in_app_options[:if])
+          ) &&
+          (
+            self.class.open_in_app_options[:unless].nil? ||
+            !send(self.class.open_in_app_options[:unless])
+          )
       end
 
       def open_in_app_url
@@ -41,12 +41,25 @@ module OpenInApp
       end
 
       def open_in_app
-        render(template: 'open_in_app', layout: false)
+        if open_in_app?
+          render(template: 'open_in_app', layout: false) and return false
+        end
+        true
       end
 
     module ClassMethods
       def open_in_app(options={})
-        before_filter OpenInApp::Filter, options
+        open_in_app_options.merge!(options.reject { |key,| not [:if, :unless].include?(key.to_sym) })
+        filter_options = options.reject { |key,| [:if, :unless, :prepend].include?(key.to_sym) }
+        if prepend_filter = options.delete(:prepend)
+          prepend_before_filter prepend_filter, :open_in_app, filter_options
+        else
+          before_filter :open_in_app, filter_options
+        end
+      end
+
+      def open_in_app_options
+        @open_in_app_options ||= {}
       end
     end
   end
